d5e629a0e194023cd5df8cace2fcf3153447a237
Maxime St-Pierre <maxime.st-pierre.3@ens.etsmtl.ca>
2016-08-22 22:31:27 -0400

    Added base views

--- django/metaclub/member/views.py
+++ django/metaclub/member/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+class ViewList():
+    pass
+
+
+class ViewMember():
+    pass
+
+
+class CreateMember():
+    pass
+
+
+def delete_member():
+    pass
+
+
+def UpdateMember():
+    pass
